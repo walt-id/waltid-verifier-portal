1c247bd027f0bd6d0d340a2c125b20c0febbb4d0
Othmane N <otmannj56@gmail.com>
2021-12-06 16:11:43 +0100

    Fixing Footer/Header|

--- nuxt.config.js
+++ nuxt.config.js
@@ -22,6 +22,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '@/plugins/bootstrap',
+    '@/plugins/bootstrap-script'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -44,7 +46,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
-    proxy: true
+    //proxy: true
   },
 
   proxy: {
